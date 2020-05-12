532f292df04e04ac9fb5affc6d8a215d461e547a
John Carter <johncarter352@gmail.com>
2020-05-12 14:51:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -30,8 +30,6 @@ class Scraper
         students_hash[:github_url] = url if url.include?("github")
         students_hash[:blog_url] = url if student.css("img").attribute("src").text.include?("rss")
     end
-        
-        
       students_hash[:profile_quote] = html.css("div.profile-quote").text
       students_hash[:bio] = html.css("div.bio-content p").text
       students_hash
