37dcbe4431dd773d500aa9d9a9413df400f9a651
John Carter <johncarter352@gmail.com>
2020-05-12 14:53:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,37 +4,49 @@ require 'nokogiri'
 
 class Scraper
 
- def self.scrape_index_page(index_url)
-
-    students_hash = []
-    html = Nokogiri::HTML(open(index_url))
-    html.css(".student-card").collect do |student|
-      hash = {
-        name: student.css("h4.student-name").text,
-        location: student.css("p.student-location").text,
-        profile_url: "http://students.learn.co/" + student.css("a").attribute("href")
-      }
-      students_hash << hash
-    end
-    students_hash
+  def self.scrape_index_page(index_url)
+    html = open('fixtures/student-site/index.html')
+    doc = Nokogiri::HTML(html)
+    scraped_students = []
+    
+    students = doc.css(".student-card")
+    students.each do |student|
+      name = student.css(".student-name").text
+      location = student.css(".student-location").text
+      profile_url = student.css("a").attr("href").text
+      hash = {:name => name,
+      :location => location,
+      :profile_url => profile_url
+    }
+    scraped_students << hash
   end
-
+    scraped_students
+  end
 
   def self.scrape_profile_page(profile_url)
-    students_hash = {}
-  
-    html = Nokogiri::HTML(open(profile_url))
-    html.css("div.social-icon-controler a").each do |student|
-        url = student.attribute("href")
-        students_hash[:twitter_url] = url if url.include?("twitter")
-        students_hash[:linkedin_url] = url if url.include?("linkedin")
-        students_hash[:github_url] = url if url.include?("github")
-        students_hash[:blog_url] = url if student.css("img").attribute("src").text.include?("rss")
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    
+    student_profiles = {}
+   
+    social_link = doc.css(".vitals-container .social-icon-container a")
+    
+    social_link.each do |element|
+      if element.attr("href").include?("twitter")
+        student_profiles[:twitter] = element.attr('href')
+      elsif element.attr("href").include?("linkedin")
+        student_profiles[:linkedin] = element.attr("href")
+      elsif element.attr("href").include?("github")
+        student_profiles[:github] = element.attr("href")
+      elsif element.attr("href").include?("com/")
+      student_profiles[:blog] = element.attr("href")
+      end
     end
-      students_hash[:profile_quote] = html.css("div.profile-quote").text
-      students_hash[:bio] = html.css("div.bio-content p").text
-      students_hash
+    
+    student_profiles[:profile_quote] = doc.css(".vitals-container .vitals-text-container .profile-quote").text
+    student_profiles[:bio] = doc.css(".bio-block.details-block .bio-content.content-holder .description-holder p").text
+    
+    student_profiles
   end
-  
 end
 
